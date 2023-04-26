afd6c03b53f15c6e9889eacd060bb201f9cc5958
tomimirad <tomimiranda1403@gmail.com>
2023-04-26 12:39:58 -0300

    se realizo la config de la DB

---
+++ src/database/models/PRODUCTO.js
@@ -0,0 +1,71 @@
+module.exports=(sequelize,DataTypes)=>{
+    let Producto =  sequelize.define("Producto",{
+            producto_id:{
+                autoIncrement:true,
+                type:DataTypes.INTEGER,
+                primaryKey: true
+            },
+
+    },{tableName:"productos"})
+    return Producto
+}
+
+// module.exports = (sequelize, DataTypes) => {
+//   let alias = 'Product'
+
+//   let cols = {
+//     id: {
+//       type: DataTypes.INTEGER(11),
+//       primaryKey: true,
+//       autoIncrement: true,
+//     },
+//     productName: {
+//       type: DataTypes.STRING(255),
+//     },
+//     price: {
+//       type: DataTypes.INTEGER(11),
+//     },
+
+//     description: {
+//       type: DataTypes.TEXT,
+//     },
+//     img: {
+//       type: DataTypes.STRING(255),
+//     },
+
+//     category: {
+//       type: DataTypes.STRING(50),
+//     },
+
+//     color: {
+//       type: DataTypes.STRING(50),
+//     },
+//     brand: {
+//       type: DataTypes.STRING(50),
+//     },
+//     size: {
+//       type: DataTypes.INTEGER(11),
+//     },
+
+//     createdAt: {
+//       type: DataTypes.DATE,
+//     },
+
+//     updatedAt: {
+//       type: DataTypes.DATE,
+//     },
+
+//     deletedAt: {
+//       type: DataTypes.DATE,
+//     },
+//   }
+
+//   let config = {
+//     tableName: 'products',
+//     paranoid: true
+//   }
+
+//   let Product = sequelize.define(alias, cols, config)
+
+// return Product
+// }
