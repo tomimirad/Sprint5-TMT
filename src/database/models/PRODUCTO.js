449c2d59676e73748d7d96136fb4673eb6c51d13
tomimirad <tomimiranda1403@gmail.com>
2023-04-30 17:02:53 -0300

    se agrego la configuracion de la tabla de productos

--- src/database/models/PRODUCTO.js
+++ src/database/models/PRODUCTO.js
@@ -1,71 +1,44 @@
-module.exports=(sequelize,DataTypes)=>{
-    let Producto =  sequelize.define("Producto",{
-            producto_id:{
-                autoIncrement:true,
-                type:DataTypes.INTEGER,
-                primaryKey: true
-            },
+module.exports = (sequelize,DataTypes)=>{
+    let alias = 'Producto'
 
-    },{tableName:"productos"})
+    let cols = {
+        producto_id:{
+            autoIncrement:true,
+            type:DataTypes.INTEGER(11),
+            primaryKey: true
+        },
+        titulo:{
+            type:DataTypes.VARCHAR(100),
+        },
+        precio:{
+            type:DataTypes.FLOAT(12),
+        },
+        descripcion:{
+            type:DataTypes.TEXT,
+        },
+        img:{
+            type:DataTypes.VARCHAR(255),
+        },
+        descuento:{
+            type:DataTypes.INTEGER(11)
+        },
+        coutas:{
+            type:DataTypes.INTEGER(11)
+        },
+        subCategoria_id:{
+            type:DataTypes.INTEGER(11)
+        },
+        sale:{
+            type:DataTypes.VARCHAR(10)
+        }
+
+    }
+
+    let config = {
+        tableName:'productos',
+        paranoid: true
+    }
+
+    let Producto = sequelize.define(alias,cols,config)
     return Producto
 }
-
-// module.exports = (sequelize, DataTypes) => {
-//   let alias = 'Product'
-
-//   let cols = {
-//     id: {
-//       type: DataTypes.INTEGER(11),
-//       primaryKey: true,
-//       autoIncrement: true,
-//     },
-//     productName: {
-//       type: DataTypes.STRING(255),
-//     },
-//     price: {
-//       type: DataTypes.INTEGER(11),
-//     },
-
-//     description: {
-//       type: DataTypes.TEXT,
-//     },
-//     img: {
-//       type: DataTypes.STRING(255),
-//     },
-
-//     category: {
-//       type: DataTypes.STRING(50),
-//     },
-
-//     color: {
-//       type: DataTypes.STRING(50),
-//     },
-//     brand: {
-//       type: DataTypes.STRING(50),
-//     },
-//     size: {
-//       type: DataTypes.INTEGER(11),
-//     },
-
-//     createdAt: {
-//       type: DataTypes.DATE,
-//     },
-
-//     updatedAt: {
-//       type: DataTypes.DATE,
-//     },
-
-//     deletedAt: {
-//       type: DataTypes.DATE,
-//     },
-//   }
-
-//   let config = {
-//     tableName: 'products',
-//     paranoid: true
-//   }
-
-//   let Product = sequelize.define(alias, cols, config)
-
-// return Product
-// }
